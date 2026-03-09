8d55d9a4f518eaa5de86c01582e6b91bfa352d6e
Raj Venkat R <rajvenkatr@Rajs-MacBook-Air-2.local>
2026-03-09 22:04:45 +0530

    SetUp

---
+++ devshell-backend/src/main/java/com/devshell/devshell_backend/DevshellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devshell.devshell_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevshellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevshellBackendApplication.class, args);
+	}
+
+}
---
+++ devshell-backend/src/test/java/com/devshell/devshell_backend/DevshellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devshell.devshell_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevshellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
